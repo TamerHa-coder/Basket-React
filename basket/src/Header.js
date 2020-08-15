7a3d65b85ff3093c1b8abb95c933949c7e89c206
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-08-15 10:50:33 +0300

    Addind Header

---
+++ basket/src/Header.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+
+
+function Header(props) {
+
+
+
+  return (
+    <div className='App-header'>
+      <img src='https://www.iconsdb.com/icons/preview/white/shopping-basket-xxl.png' width="150" height="150" />
+      <h1  >Hello, Basket!</h1>
+    </div>
+  );
+}
+export default Header;
